a8404f260e9f00dff2c06bfe3af122a78e93fc7f
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 15:50:55 -0300

    Add project files.

---
+++ ValidadorDeSenha/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValidadorDeSenha
+{
+    class Program
+    {
+        /// <summary>
+        /// A senha correta é 2002
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            bool senhaValida = false;
+            string senha;
+
+            while (!senhaValida)
+            {
+                Console.Write("Digite a senha: ");
+                senha = Console.ReadLine();
+
+                if (senha.Equals("2002"))
+                    senhaValida = true;
+                else
+                    Console.WriteLine("Senha inválida");
+            }
+
+            Console.Write("Acesso Permitido");
+        }
+    }
+}
